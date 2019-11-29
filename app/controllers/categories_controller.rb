20e214ad7c50f2697c104fe928f0cc94f9d9b710
DennisDaddy <softteez95@gmail.com>
2019-11-29 07:28:45 +0300

    Update categories views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,49 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def index
+    @category = Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save     
+      flash[:success] = "Successfuly created!"
+      redirect_to categories_path
+    else
+      flash[:danger] = "Failed or login as admin to continue!"
+      redirect_to new_category_path
+    end    
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:success] = "Successfuly updated!"    
+    redirect_to categories_path
+    else
+      render 'edit'
+    end
   end
 
   def edit
   end
 
   def show
+    @category = Category.find(params[:id])
   end
   
-  def destroy    
+  def destroy
+    Category.find(params[:id]).destroy
+    flash[:success] = "Category deleted!"
+    redirect_to categories_url
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
