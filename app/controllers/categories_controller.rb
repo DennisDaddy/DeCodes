40bbc41bdc1170ae7adb47d397b014ce3c02ebd9
DennisDaddy <softteez95@gmail.com>
2019-11-29 07:05:56 +0300

    Generate categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+  
+  def destroy    
+  end
+end
